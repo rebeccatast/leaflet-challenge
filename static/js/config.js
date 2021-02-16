3a8974cacb752635318351ec261c73ea777f004d
Rebecca Tast <69356535+rebeccatast@users.noreply.github.com>
2021-02-16 16:54:40 -0600

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoicmViZWNjYXRhc3QiLCJhIjoiY2trcmp4eWFiMGQ2aTJxbGo4eXNhNzZrdiJ9.twrUlHxwx0plfQZiJdvFSg";
+const API_KEY = "pk.eyJ1IjoicmViZWNjYXRhc3QiLCJhIjoiY2tsOGtsaG1oMjMxYTJubW1raWIxZWhtbCJ9.fn7MsC5iyxCFanNm5-wbXA";

d02e4ac65b0aba89a199fab71f4e4c0ae5e44870
Rebecca Tast <69356535+rebeccatast@users.noreply.github.com>
2021-02-20 11:14:37 -0600

    Update logic1.js

--- static/js/logic1.js
+++ static/js/logic1.js
@@ -27,31 +27,31 @@ d3.json(URL, function (data) {
     return {
       opacity: 1,
       fillOpacity: 1,
-      fillColor: getColor(feature.properties.mag),
+      fillColor: getColor(feature.geometry.coordinates[2]),
       color: "#000000",
       radius: getRadius(feature.properties.mag),
       stroke: true,
       weight: 0.5
     };
   }
-  // set different color from magnitude
-  function getColor(magnitude) {
+  // set different color for depth
+  function getColor(depth) {
     switch (true) {
-      case magnitude > 5:
+      case depth > 90:
         return "#ea2c2c";
-      case magnitude > 4:
+      case depth > 70:
         return "#ea822c";
-      case magnitude > 3:
+      case depth > 50:
         return "#ee9c00";
-      case magnitude > 2:
+      case depth > 30:
         return "#eecc00";
-      case magnitude > 1:
-        return "#d4ee00";
+      case depth > 10:
+        return "#4dee00";
       default:
         return "#98ee00";
     }
   }
-  // set radiuss from magnitude
+  // set radius from magnitude
   function getRadius(magnitude) {
     if (magnitude === 0) {
       return 1;
@@ -67,12 +67,12 @@ d3.json(URL, function (data) {
     style: styleInfo,
     onEachFeature: function (feature, layer) {
       layer.bindPopup(
-        "Magnitude: "
+        "Location: "
+        + feature.properties.place
+        + "<br>Magnitude: "
         + feature.properties.mag
         + "<br>Depth: "
         + feature.geometry.coordinates[2]
-        + "<br>Location: "
-        + feature.properties.place
       );
     }
     //Add the data to the earthquake layer
@@ -87,21 +87,13 @@ d3.json(URL, function (data) {
   legend.onAdd = function () {
     var div = L.DomUtil.create("div", "info legend");
 
-    var grades = [0, 1, 2, 3, 4, 5];
-    var colors = [
-      "#98ee00",
-      "#d4ee00",
-      "#eecc00",
-      "#ee9c00",
-      "#ea822c",
-      "#ea2c2c"
-    ];
+    var grades = [-10, 10, 30, 50, 70, 90]
 
     // Looping through
     for (var i = 0; i < grades.length; i++) {
       div.innerHTML +=
-        "<i style='background: " + colors[i] + "'></i> " +
-        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+        "<i style='background: " + getColor[i] + "'></i> " +
+        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : '+');
     }
     return div;
   };
@@ -109,3 +101,4 @@ d3.json(URL, function (data) {
   // Finally, we our legend to the map.
   legend.addTo(myMap);
 });
+
